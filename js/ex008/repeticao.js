6e68c1719ee14f40367e53e1ea0b50be58aee932
justa-whitefox <vitor.sampaio2000@hotmail.com>
2021-09-30 15:30:42 -0300

    js

---
+++ js/ex008/repeticao.js
@@ -0,0 +1,15 @@
+//console.log('good day sir');
+
+var count = 1;
+
+/*
+while (count <= 50) {
+    console.log(`good day sir ${count}`);
+    count++
+}
+*/
+
+do {
+    console.log('good day sir');
+    count++
+} while (count <= 10);
