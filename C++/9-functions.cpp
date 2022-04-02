f71884e088bf40a18552d90bc9e22bac162f65c4
justa-whitefox <justa.redfox2002@gmail.com>
2022-04-02 12:18:49 -0300

    update

--- C++/9-functions.cpp
+++ C++/9-functions.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void greatings() { // a function is a block of code that can be 'called' multiple times
+    cout << "hello, whoever is seen this!" << endl;
+}
+
+void multipleParameters(string name, int age) { //a function can receive multiple parameters
+    cout << name << " you are " << age << " years old?" << endl;
+}
+
+void functionWithVariable(string firstName); //to use a function that is after the main function we need to 'declare' it first, before the main function
+
 int main() {
+    string firstName; //calling a function
+
+    greatings();
+    multipleParameters("john", 19); //calling a function with parameters
+
+    cout << "what is your name?\n >>" << endl;
+    cin >> firstName;
+
+    functionWithVariable(firstName); //calling a function with variables
 
     return 0;
+}
+
+void functionWithVariable(string firstName) {
+    cout << "your name is: " << firstName << " ?" << endl;
 }
