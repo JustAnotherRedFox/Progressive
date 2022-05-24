91a8457ec8a86eda30c4c93832db9e3ba8c3935e
justa-whitefox <justa.redfox2002@gmail.com>
2022-05-24 18:55:37 -0300

    openGL

--- OpenGL/OpenGL_Cherno/OpenGL_Cherno/src/Application.cpp
+++ OpenGL/OpenGL_Cherno/OpenGL_Cherno/src/Application.cpp
@@ -1,6 +1,46 @@
 #include <GL/glew.h> // glew has to be include before every OpenGL lib
 #include <GLFW/glfw3.h>
 #include <iostream>
+using std::string;
+
+static unsigned int CompileShader(unsigned int type, const string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Fail to compile " << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << " Shader." << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const string& vertexShader, const string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -28,14 +68,39 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
+        -0.5f, -0.5f, // 1st vertex
+         0.0f,  0.5f, //2nd vertex
          0.5f, -0.5f
     };
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    string vertexShader =
+        "#version 310 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main() {\n"
+        "   gl_Position = position;\n"
+        "}\n"
+        ;
+
+    string fragmentShader =
+        "#version 310 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main() {\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n" //RGBA(Red, Green, Blue, Alpha), 0.0 = 0, 1.0 = 255
+        "}\n"
+        ;
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
